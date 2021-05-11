4608bc348d8205515d7cd8e96a8d8d47a7e7ba9c
leidy <leidylauraverasveras@gmail.com>
2021-05-11 16:23:49 -0400

    Setup sign in with Google

--- src/App.js
+++ src/App.js
@@ -14,8 +14,37 @@ import {
  } from './config/firebase';
 
  function App() {
-   return (
-    <div></div>
-   );
-   }
-   export default App;
+   
+  const signInWithGoogle = async () => {
+
+    // Coloca el lenguaje de preferencia del dispocitivo 
+    firebase.auth() .useDeviceLanguage();
+
+    // Incio el procedo de login dentro de un try ... catch 
+
+    try{
+      await firebase.auth() .signInWithPopup(googleAuthprovider);
+
+ } catch (e) {
+   console.error(e.message);
+    { 
+
+   };
+
+   // Renderizar el Componente Button 
+   return(
+
+    import {
+      db,
+      googleAuthprovider,
+      firebase
+     } from './config/firebase';
+     import Button from './components/Button';
+
+     return (
+      <div>
+         <Button onclick= {signInWithGoogle} > sign in with Google</Button>
+         </div>
+     );
+     }
+     export default App ;
---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+const Button = ({ 
+    onclick = null,
+    children = null
+ }) =>
+ (
+    <button onclick={onclick} >{children} </button>
+ );
+
+ export default Button;
+ 
